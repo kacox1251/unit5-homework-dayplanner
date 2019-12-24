d7b94217cd183ce321dc5d7393c2fda96725cacc
kacox1251 <56182547+kacox1251@users.noreply.github.com>
2019-12-23 22:03:40 -0700

    commit 4

--- script.js
+++ script.js
@@ -1,48 +1,67 @@
-var hours = [8, 
-            9,
-            10,
-            11,
-            12,
-            13,
-            14,
-            15,
-            16,
-            17,
-            18];
-
+var defaultStoredPlans = {
+    8: null,
+    9: null,
+    10: null,
+    11: null,
+    12: null,
+    13: null,
+    14: null,
+    15: null,
+    16: null,
+    17: null,
+    18: null
+  };
+var hours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 var currentDay = $("#currentDay");
+var now = moment();
+var inputs = $(".textarea");
+var saveBtn = $(".saveBtn");
 
-console.log(moment());
-
-var now = moment().format("dddd, MMMM Do YYYY");
-
-$("#currentDay").text(now);
-console.log(now);
-
-console.log(moment().hour());
-
-for (i = 0; i < hours.length; i++) {
-    if (moment().hour() === hours[i]) {
-        $(".textarea").addClass("present");
-    }
-    
-    else {
-        $(".textarea").addClass("future");
-    }
+function convertIdToTime(id) {
+    return moment().format("MM/DD/YYYY") + " " + id + ":00";
 }
 
+var storedPlans = localStorage.getItem("Plans");
+if (storedPlans) {
+    storedPlans = JSON.parse(storedPlans);
+} else {
+    storedPlans = defaultStoredPlans;
+}
+  
+currentDay.text(now.format("dddd, MMMM Do YYYY"));
+  
 
-    // else if (moment().isSame(hours[i], "hour")) {
-    //     $(".textarea").addClass("present");
-    // }
-var saveBtn = $(".saveBtn");
+  
+for (i = 0; i < inputs.length; i++) {
+    var input = inputs[i];
+    var id = $(input).attr("id");
+    var time = moment(convertIdToTime(id));
+    if (parseInt(id) === now.hour()) {
+      $(input).addClass("present");
+    } else if (time.isBefore(now)) {
+      $(input).addClass("past");
+    } else {
+      $(input).addClass("future");
+    }
+}
+  
 
 saveBtn.on("click", function(event) {
     event.preventDefault();
-    var plans = $(".textarea").val();
-    localStorage.setItem("Plans", plans);
-})
-
-var savedPlans = localStorage.getItem("Plans");
-$(".textarea").textContent = savedPlans;
-console.log(savedPlans);
+    var plans = $(this)
+      .siblings("input")
+      .val();
+    var planTime = $(this)
+      .siblings("input")
+      .attr("id");
+    storedPlans[planTime] = plans;
+    var newPlan = JSON.stringify(storedPlans);
+    localStorage.setItem("Plans", newPlan);
+});
+  
+$(".textarea").each(function(index, element) {
+    var element = $(element);
+    var key = element.attr("id");
+    element.val(storedPlans[key]);
+});
+  
